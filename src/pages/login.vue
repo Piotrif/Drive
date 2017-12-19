21dc7a51bf21d9946bfaca6fc9b823aab71edb25
Jonas Christensen (JONC) <JONC@NIRAS.DK>
2017-12-19 15:59:25 +0100

    added svg and scss components

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="content">
-        <h1>Velkommen</h1>
+    <div class="login flexbox-center flex-row">
+        <div class="flex-div login-left">
+            <h1>Velkommen</h1>
+
+        </div>
+
+        <div class="flex-div login-right">
+            <h1>Billeder</h1>
+        </div>
     </div>
 </template>
 
@@ -14,7 +21,3 @@ export default {
     }
 }
 </script>
-
-<style>
-
-</style>
